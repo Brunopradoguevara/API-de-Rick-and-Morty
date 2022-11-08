4358d9b8854a6c3bb4e6c2ccebfe50b3a1f68ba5
casabugambiliascancun <casabugambiliascancun@gmail.com>
2022-11-08 13:50:44 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function getCharacters(done){
+  const results = fetch("https://rickandmortyapi.com/api/character");
+
+  results.then(response=>response.json())
+          .then(data=>{
+            done(data)
+          });
+}
+
+
+getCharacters(data=>{
+     data.results.forEach(personaje => {
+      const articule = document.createRange().createContextualFragment(/**html */ `
+      <article>
+            <div class="image-container">
+                <img src="${personaje.image}" alt="personaje">
+            </div>
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+        </article>
+      `)  
+        
+      const main = document.querySelector("main");
+
+      main.append(articule);
+
+     });
+});
